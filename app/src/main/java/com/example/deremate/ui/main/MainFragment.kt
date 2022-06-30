bc2b52cdc89f25d193ffaf7ba372b390cebb5b0e
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-06-30 18:03:38 -0300

    Initial Commit - Added Colors & Logo

---
+++ app/src/main/java/com/example/deremate/ui/main/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.deremate.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.deremate.R
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
